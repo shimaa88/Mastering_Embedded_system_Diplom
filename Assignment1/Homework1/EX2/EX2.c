08db3a1b51e90c6f39d5984e00c6ca883e20bd31
shimaa aziz <shimaaabdelaziz98@gmail.com>
2022-03-07 05:24:05 +0200

    Assighnment

---
+++ Assignment1/Homework1/EX2/EX2.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+int main()
+{int number;
+printf("Enter a integer:");
+fflush(stdout);
+scanf("%d",&number);
+printf("You entered: %d",number);
+}
---
+++ Assignment1/Homework1/EX3/EX3.c
@@ -0,0 +1,10 @@
+#include"stdio.h"
+int main()
+{int number1,number2;
+printf("Enter two integers:");
+fflush(stdout);
+scanf("%d %d",&number1,&number2);
+printf("Sum: %d",number1+number2);
+
+
+}
